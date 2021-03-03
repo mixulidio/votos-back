559c8380bbe17bddcfd623134668ae9b6b3e61a8
mixulidio <mixulidio@gmail.com>
2021-03-03 10:02:59 -0300

    configuração inicial Spring + Pom.xml + JPA

---
+++ src/main/java/com/cooperativismo/votosback/VotosBackApplication.java
@@ -0,0 +1,13 @@
+package com.cooperativismo.votosback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotosBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotosBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cooperativismo/votosback/VotosBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooperativismo.votosback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotosBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
